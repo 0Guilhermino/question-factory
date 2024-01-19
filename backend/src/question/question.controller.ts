9d0af1e405b7629a41c4e0d600514eca5b067c44
Angelo Guilhermino <angeloguilhermino@gmail.com>
2024-01-19 19:53:15 -0300

    feat(question-service):create Question

--- backend/src/question/question.controller.ts
+++ backend/src/question/question.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {QuestionService} from "./question.service";
+import {Question} from "./person.model";
 
 @Controller('question')
-export class QuestionController {}
+export class QuestionController {
+    constructor(private readonly questionService: QuestionService) {}
+
+    @Get()
+    getAllQuestions(){
+        return this.questionService.getAllQuestions()
+    }
+
+    @Get(':id')
+    getQuestionsById(@Param('id') id: number){
+        return this.questionService.getQuestionsById(id);
+    }
+
+    @Post()
+    createQuestion(@Body question: Question){
+        this.questionService.createQuestion(question);
+        return question;
+    }
+
+
+
+}
