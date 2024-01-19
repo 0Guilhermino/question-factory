22040ca74021cfaf28e7e349d3df51a394e62ced
Angelo Guilhermino <angeloguilhermino@gmail.com>
2024-01-19 19:41:27 -0300

    feat(question-service):update service methods

--- backend/src/question/question.service.ts
+++ backend/src/question/question.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import {Question} from "./person.model";
 
 @Injectable()
-export class QuestionService {}
+export class QuestionService {
+    private questions: Question[] = []
+
+    getAllQuestions() {
+        return this.questions;
+    }
+
+    getQuestionsById(id: number) {
+        return this.questions.find(question => question.id === id);
+    }
+
+    createQuestion(question: Question){
+        this.questions.push(question);
+    }
+
+    updateQuestion(updatedQuestion: Question) {
+        const index = this.questions.findIndex(question => question.id === updatedQuestion.id)
+        if (index !== -1){
+            this.questions[index] = updatedQuestion;
+        }
+    }
+
+    deleteQuestions(questionId: number){
+        this.questions = this.questions.filter(question => question.id !== questionId);
+    }
+
+
+}
